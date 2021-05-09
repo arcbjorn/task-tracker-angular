7ddde51a7d5c1ca60388b4ea8acbaf48ddebb245
arcbjorn <arky.happ@gmail.com>
2021-05-09 15:08:00 +0300

    feat (src/app/components): add Task Item

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ITask } from 'src/app/types';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css'],
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task!: ITask;
+  faTimes = faTimes;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
